ca6e9bc7d38a6f5e72794980e899d0ed25986ef2
giraftw2002 <giraftw2002@gmail.com>
2020-08-06 16:54:12 +0800

    add odom

--- myrosbot.ino
+++ myrosbot.ino
@@ -9,12 +9,21 @@
 */
 
 #include <ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/tf.h>
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 
 #include "TimerOne.h"
 #include <PID_v1.h>
+
+/*
+ * Declaration Globales
+ */
+geometry_msgs::Range range_msg;
+nav_msgs::Odometry odom;
 
 #define PI 3.14159265359
 /*******************************************************************************
@@ -60,14 +69,15 @@ volatile uint16_t rpmL, rpmR;
 #define DEG2RAD(x) (x * 0.01745329252) // *PI/180
 #define RAD2DEG(x) (x * 57.2957795131) // *180/PI
 #define tickPerMeter ((2.0 * PI * r) / SPOKE)
-struct xy_theta
+typedef struct
 {
-  int x;
-  int y;
-  int theta;
-};
-xy_theta pose;
+    int x;
+    int y;
+    int theta;
+} xy_theta;
 
+xy_theta pose;
+double dC=0, theta = 0;
 //Specify the links and initial tuning parameters
 // kpL and kpR?
 double Kp = 0.53, Ki = 0.14, Kd = 0;
@@ -76,49 +86,53 @@ PID myPidR(&InputR, &OutputR, &SetpointR, Kp, Ki, Kd, DIRECT);
 
 void encoderL() // counts from the speed sensor
 {
-  counterL++; // increase +1 the counter value
+    counterL++; // increase +1 the counter value
 }
 
 void encoderR() // counts from the speed sensor
 {
-  counterR++; // increase +1 the counter value
+    counterR++; // increase +1 the counter value
 }
 
 void timerIsr()
 {
-  float dC, dL, dR;
+    double dL, dR;
 
-  Timer1.detachInterrupt(); //stop the timer
-  rpmL = counterL * 3;      // (counter/spoke) * 60
-  rpmR = counterR * 3;
-  dL = counterL * tickPerMeter;
-  dR = counterR * tickPerMeter;
-  counterL = counterR = 0; //  reset counter to zero asap
+    Timer1.detachInterrupt(); //stop the timer
+    rpmL = counterL * 3;      // (counter/spoke) * 60
+    rpmR = counterR * 3;
+    dL = counterL * tickPerMeter;
+    dR = counterR * tickPerMeter;
+    counterL = counterR = 0; //  reset counter to zero asap
 
-  dC = (dR + dL) / 2;
-  pose.x = pose.x + (dC * cos(pose.theta));
-  pose.y = pose.y + (dC * cos(pose.theta));
-  pose.theta = pose.theta + (dR - dL) / L;
-  // constrain theta in [-pi, pi]
-  pose.theta = atan2(sin(pose.theta), cos(pose.theta));
+    dC = (dR + dL) / 2.0;
+    pose.x += dC * cos(pose.theta);
+    pose.y += dC * sin(pose.theta);
+    theta = (dR - dL) / L;
+    pose.theta += theta;
+    // constrain theta in [-pi, pi]
+    pose.theta = atan2(sin(pose.theta), cos(pose.theta));
 
-  Timer1.attachInterrupt(timerIsr); //enable the timer
+    Timer1.attachInterrupt(timerIsr); //enable the timer
 }
 
 void cmd_velCb(const geometry_msgs::Twist &cmd_vel)
 {
-  double v, w;
-  v = cmd_vel.linear.x;
-  w = cmd_vel.angular.z;
- 
-  // uni_to_diff to RPM
-  SetpointL = (((2 * v) - (w * L)) / (2 * r)) * 9.5493;
-  SetpointR = (((2 * v) + (w * L)) / (2 * r)) * 9.5493;
+    double v, w;
+    v = cmd_vel.linear.x;
+    w = cmd_vel.angular.z;
+
+    // uni_to_diff to RPM
+    SetpointL = (((2.0 * v) - (w * L)) / (2.0 * r)) * 9.5493;
+    SetpointR = (((2.0 * v) + (w * L)) / (2.0 * r)) * 9.5493;
 }
 
 ros::NodeHandle nh;
-sensor_msgs::Range range_msg;
+geometry_msgs::TransformStamped t;
+tf::TransformBroadcaster broadcaster;
+
 ros::Publisher pub_range("range_data", &range_msg);
+ros::Publisher pub_odom("Odometry", &odom);
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &cmd_velCb);
 
 unsigned long range_timer;
@@ -132,125 +146,138 @@ unsigned long range_timer;
 */
 int getRange()
 {
-  digitalWrite(TRIG, HIGH);
-  digitalWrite(TRIG, LOW);
-  int dist = pulseIn(ECHO, HIGH) / 50;
-  return dist;
+    digitalWrite(TRIG, HIGH);
+    digitalWrite(TRIG, LOW);
+    int dist = pulseIn(ECHO, HIGH) / 50;
+    return dist;
 }
 
 void setup()
 {
-  // Serial.begin(57600);
+    // Serial.begin(57600);
 
-  // init ros
-  nh.initNode();
-  // nh.advertise(pub_range);
-  nh.subscribe(sub);
+    // init ros
+    nh.initNode();
+    // nh.advertise(pub_range);
+    nh.advertise(pub_odom);
+    nh.subscribe(sub);
 
-  /* init ir_ranger
-    pinMode(TRIG, OUTPUT);
-    pinMode(ECHO, INPUT);
-    range_timer = 0;
-    range_msg.radiation_type = sensor_msgs::Range::INFRARED;
-    range_msg.header.frame_id = "utlrasound";
-    range_msg.field_of_view = 0.10000000149;
-    range_msg.min_range = 0.02;
-    range_msg.max_range = 4; // in meter
-  */
+    /* init ir_ranger
+      pinMode(TRIG, OUTPUT);
+      pinMode(ECHO, INPUT);
+      range_timer = 0;
+      range_msg.radiation_type = sensor_msgs::Range::INFRARED;
+      range_msg.header.frame_id = "utlrasound";
+      range_msg.field_of_view = 0.10000000149;
+      range_msg.min_range = 0.02;
+      range_msg.max_range = 4; // in meter
+    */
+    // init odom
+    odom.header.frame_id="/odom");
+    odom.child_frame_id = "/base_link";
 
-  // init motors
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  rpmL = rpmR = counterL = counterR = 0;
-  pose.x = pose.y = pose.theta = 0;
+    // init motors
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    rpmL = rpmR = counterL = counterR = 0;
+    pose.x = pose.y = pose.theta = 0;
 
-  //  init encoders
-  pinMode(interruptPinL, INPUT_PULLUP);
-  pinMode(interruptPinR, INPUT_PULLUP);
-  Timer1.initialize(1000000);                                              // set timer for 1sec
-  attachInterrupt(digitalPinToInterrupt(interruptPinL), encoderL, RISING); // increase counter when speed sensor pin goes High
-  attachInterrupt(digitalPinToInterrupt(interruptPinR), encoderR, RISING); // increase counter when speed sensor pin goes High
-  Timer1.attachInterrupt(timerIsr);                                        // enable the timer
+    //  init encoders
+    pinMode(interruptPinL, INPUT_PULLUP);
+    pinMode(interruptPinR, INPUT_PULLUP);
+    Timer1.initialize(1000000);                                              // set timer for 1sec
+    attachInterrupt(digitalPinToInterrupt(interruptPinL), encoderL, RISING); // increase counter when speed sensor pin goes High
+    attachInterrupt(digitalPinToInterrupt(interruptPinR), encoderR, RISING); // increase counter when speed sensor pin goes High
+    Timer1.attachInterrupt(timerIsr);                                        // enable the timer
 
-  //  init PIDs
-  myPidL.SetSampleTime(1000); // adj this number to half makes difference. why?
-  myPidL.SetMode(AUTOMATIC);
-  myPidR.SetSampleTime(1000); // adj this number to half makes difference. why?
-  myPidR.SetMode(AUTOMATIC);
+    //  init PIDs
+    myPidL.SetSampleTime(1000); // adj this number to half makes difference. why?
+    myPidL.SetMode(AUTOMATIC);
+    myPidR.SetSampleTime(1000); // adj this number to half makes difference. why?
+    myPidR.SetMode(AUTOMATIC);
 
-  analogWrite(ENA, 0); // set speed of motor (0-255
-  analogWrite(ENB, 0); // set speed of motor (0-255
-  forward();
+    analogWrite(ENA, 0); // set speed of motor (0-255
+    analogWrite(ENB, 0); // set speed of motor (0-255
+    forward();
 }
 
 void loop()
 {
-  /*
-    if (Serial.available())
-    {
-    v = Serial.parseInt();
-    if (Serial.peek() == '\n')
-      Serial.read(); // consume LF
-    Serial.print("Set to: ");
-    SetpointL = calSetpointL(v / 10, 0);
-    SetpointR = calSetpointR(v / 10, 0);
-    Serial.println(SetpointL);
-    analogWrite(ENA, (int)SetpointL);
-    analogWrite(ENB, (int)SetpointL);
-    }
-    setpoint are set in cmd_velCb
-    SetpointL = calSetpointL(v, w);
-    SetpointR = calSetpointR(v, w);
-  */
+    /*
+      if (Serial.available())
+      {
+      v = Serial.parseInt();
+      if (Serial.peek() == '\n')
+        Serial.read(); // consume LF
+      Serial.print("Set to: ");
+      SetpointL = calSetpointL(v / 10, 0);
+      SetpointR = calSetpointR(v / 10, 0);
+      Serial.println(SetpointL);
+      analogWrite(ENA, (int)SetpointL);
+      analogWrite(ENB, (int)SetpointL);
+      }
+      setpoint are set in cmd_velCb
+      SetpointL = calSetpointL(v, w);
+      SetpointR = calSetpointR(v, w);
+    */
 
-  /* publish the range value every 50 milliseconds
-    //   since it takes that long for the sensor to stabilize
-    if ((millis() - range_timer) > 50)
-    {
-    range_msg.range = getRange();
-    range_msg.header.stamp = nh.now();
-    pub_range.publish(&range_msg);
-    range_timer = millis();
-    }
-  */
+    // publish the range value every 50 milliseconds
+      //   since it takes that long for the sensor to stabilize
+      if ((millis() - range_timer) > 100)
+      {
+      /*
+      range_msg.range = getRange();
+      range_msg.header.stamp = nh.now();
+      pub_range.publish(&range_msg);
+      
+      */
+      t.transform.translation.x = pose.x;
+      t.transform.translation.y = pose.y;
+      t.transform.rotation = tf::createQuaternionFromYaw(pose.theta);
 
-  InputL = rpmL;
-  InputR = rpmR;
-  /*
-    Serial.print("inputL: ");
-    Serial.println(InputL, DEC);
-    Serial.print("inputR: ");
-    Serial.println(InputR, DEC);
-  */
-  myPidL.Compute();
-  myPidR.Compute();
-  analogWrite(ENA, (int)OutputL);
-  analogWrite(ENB, (int)OutputR);
-  /*
-    Serial.print("outPutL: ");
-    Serial.println(OutputL, DEC);
-    Serial.print("OutputR: ");
-    Serial.println(OutputR, DEC);
-  */
-  nh.spinOnce();
-  delay(10);
+      odom.header.stamp = nh.now();
+      odom.pose.pose.position.x = pose.x;
+      odom.pose.pose.position.y = pose.y;
+      odom.pose.pose.position.z = pose.theta;
+      odom.pose.pose.orientation = odom_quat;
+
+      // output Velocity
+      odom.twist.twist.linear.x = dC;
+      odom.twist.twist.linear.y = 0;
+      odom.twist.twist.angular.z = theta;
+      pub_odom.publish(&odom);
+      broadcaster.sendTransform(t);
+      range_timer = millis();
+      }
+    
+
+    InputL = rpmL;
+    InputR = rpmR;
+    /*
+      Serial.print("inputL: ");
+      Serial.println(InputL, DEC);
+      Serial.print("inputR: ");
+      Serial.println(InputR, DEC);
+    */
+    myPidL.Compute();
+    myPidR.Compute();
+    analogWrite(ENA, (int)OutputL);
+    analogWrite(ENB, (int)OutputR);
+    /*
+      Serial.print("outPutL: ");
+      Serial.println(OutputL, DEC);
+      Serial.print("OutputR: ");
+      Serial.println(OutputR, DEC);
+    */
+   
+    nh.spinOnce();
+    delay(10);
 }
-
-/* input v in m/s, ret rpm
-double calSetpointL(double v, double omega)
-{
-  double Vl;
-
-  Vl = ((2 * v) - (omega * L)) / (2 * r);
-  // RPM = (60/2*pi*r)*v
-  // rpmR = Vr / 0.10472;  // new v linear is Vr*r = m/s
-  return (Vl / 0.10472);
-}
-
+/*
 double calSetpointR(double v, double omega)
 {
   double Vr;
@@ -263,15 +290,15 @@ double calSetpointR(double v, double omega)
 
 void stop()
 {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
 void forward()
 {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
